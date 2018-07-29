cc4273dcd02cbdc8f461d56c3f71de8cc5d6ffce
Brad Said <bradsaid@gmail.com>
2018-07-29 15:09:43 -0500

    heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
